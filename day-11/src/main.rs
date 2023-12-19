38f13a244feb9711c2dc877b7132fdfb23b8fb31
Santiago Soler <santisoler@fastmail.com>
2023-12-18 19:17:01 -0800

    Solve part 2 of day 11

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -10,19 +10,28 @@ mod tests {
         let result = solve_part1(&fname);
         assert_eq!(result, 374);
     }
+
+    #[test]
+    fn test_solution_any_factor() {
+        let fname = String::from("data/test_input");
+        let result = solution(&fname, &10);
+        assert_eq!(result, 1030);
+        let result = solution(&fname, &100);
+        assert_eq!(result, 8410);
+    }
 }
 
 #[derive(Debug)]
 struct Galaxy {
-    x: u32,
-    y: u32,
+    x: u64,
+    y: u64,
 }
 
 impl Galaxy {
-    fn distance(&self, other: &Galaxy) -> u32 {
+    fn distance(&self, other: &Galaxy) -> u64 {
         let dx = (self.x as i32 - other.x as i32).abs();
         let dy = (self.y as i32 - other.y as i32).abs();
-        (dx + dy) as u32
+        (dx + dy) as u64
     }
 }
 
@@ -41,8 +50,8 @@ fn parse_file(fname: &String) -> Vec<Galaxy> {
         for (x, char) in line.chars().enumerate() {
             if char == '#' {
                 galaxies.push(Galaxy {
-                    x: x as u32,
-                    y: y as u32,
+                    x: x as u64,
+                    y: y as u64,
                 })
             }
         }
@@ -50,18 +59,18 @@ fn parse_file(fname: &String) -> Vec<Galaxy> {
     galaxies
 }
 
-fn expand_galaxies(galaxies: &mut Vec<Galaxy>) {
-    let mut xs: Vec<u32> = galaxies.iter().map(|g| g.x).collect();
+fn expand_galaxies(galaxies: &mut Vec<Galaxy>, factor: &u64) {
+    let mut xs: Vec<u64> = galaxies.iter().map(|g| g.x).collect();
     xs.sort();
-    let mut ys: Vec<u32> = galaxies.iter().map(|g| g.y).collect();
+    let mut ys: Vec<u64> = galaxies.iter().map(|g| g.y).collect();
     ys.sort();
-    let missing_x: Vec<u32> = (xs[0]..*xs.iter().last().unwrap())
-        .filter(|&x| !xs.contains(&(x as u32)))
-        .map(|x| x as u32)
+    let missing_x: Vec<u64> = (xs[0]..*xs.iter().last().unwrap())
+        .filter(|&x| !xs.contains(&(x as u64)))
+        .map(|x| x as u64)
         .collect();
-    let missing_y: Vec<u32> = (ys[0]..*ys.iter().last().unwrap())
-        .filter(|&y| !ys.contains(&(y as u32)))
-        .map(|y| y as u32)
+    let missing_y: Vec<u64> = (ys[0]..*ys.iter().last().unwrap())
+        .filter(|&y| !ys.contains(&(y as u64)))
+        .map(|y| y as u64)
         .collect();
     for galaxy in galaxies.iter_mut() {
         let n_xs = {
@@ -82,14 +91,14 @@ fn expand_galaxies(galaxies: &mut Vec<Galaxy>) {
             }
             n
         };
-        galaxy.x += n_xs;
-        galaxy.y += n_ys;
+        galaxy.x += n_xs * (factor - 1);
+        galaxy.y += n_ys * (factor - 1);
     }
 }
 
-fn solve_part1(fname: &String) -> u32 {
+fn solution(fname: &String, factor: &u64) -> u64 {
     let mut galaxies = parse_file(fname);
-    expand_galaxies(&mut galaxies);
+    expand_galaxies(&mut galaxies, &factor);
     let mut distances = 0;
     for i in 0..galaxies.len() {
         for j in i..galaxies.len() {
@@ -99,8 +108,18 @@ fn solve_part1(fname: &String) -> u32 {
     distances
 }
 
+fn solve_part1(fname: &String) -> u64 {
+    solution(fname, &2)
+}
+
+fn solve_part2(fname: &String) -> u64 {
+    solution(fname, &1_000_000)
+}
+
 fn main() {
     let fname = String::from("data/input");
     let result = solve_part1(&fname);
     println!("Solution to part 1: {}", result);
+    let result = solve_part2(&fname);
+    println!("Solution to part 2: {}", result);
 }
