b52d805df2c45aabf7ea62337acccf5dd3d3a30b
Santiago Soler <santisoler@fastmail.com>
2023-12-20 18:04:44 -0800

    Solve part 2 of day 13

--- day-13-python/main.py
+++ day-13-python/main.py
@@ -6,6 +6,11 @@ def test_part1():
     assert solve_part1(fname) == 405
 
 
+def test_part2():
+    fname = Path(__file__).parent / "data" / "test_input"
+    assert solve_part2(fname) == 400
+
+
 def read_file(fname: Path) -> list[list[list[bool]]]:
     arrays = []
     eof = False
@@ -75,6 +80,37 @@ def find_symmetry_plane(pattern, vertical=True) -> int | None:
     return planes[0]
 
 
+def pattern_to_string(pattern):
+    string = ""
+    for row in pattern:
+        string += "".join(["#" if i else "." for i in row]) + "\n"
+    return string
+
+
+def find_almost_symmetry_plane(pattern, vertical=True) -> int | None:
+    if not vertical:
+        pattern = transpose(pattern)
+
+    n_cols = len(pattern[0])
+
+    result = None
+    for plane in range(1, n_cols):
+        start, end = get_start_end(plane, n_cols)
+        p_counts = [count_non_palindromy(row[start:end]) for row in pattern]
+        if sum(p_counts) == 1:
+            result = plane
+            break
+    return result
+
+
+def count_non_palindromy(sequence):
+    """
+    Count how many pairs of elements in the sequence doesn't satisfy palindromy.
+    """
+    comparisons = [sequence[i] != sequence[-(i + 1)] for i in range(len(sequence) // 2)]
+    return sum(comparisons)
+
+
 def transpose(array: list[list]) -> list[list]:
     return [list(x) for x in zip(*array)]
 
@@ -108,7 +144,27 @@ def solve_part1(fname: Path) -> int:
     return result
 
 
+def solve_part2(fname: Path) -> int:
+    patterns = read_file(fname)
+    result = 0
+    for pattern in patterns:
+        symmetry_plane = find_almost_symmetry_plane(pattern, vertical=True)
+        if symmetry_plane is not None:
+            result += symmetry_plane
+            continue
+
+        symmetry_plane = find_almost_symmetry_plane(pattern, vertical=False)
+        if symmetry_plane is not None:
+            result += 100 * symmetry_plane
+        else:
+            raise RuntimeError("Unable to find any symmetry plane")
+
+    return result
+
+
 if __name__ == "__main__":
     fname = Path(__file__).parent / "data" / "input"
     result = solve_part1(fname)
     print(f"Solution to part 1: {result}")
+    result = solve_part2(fname)
+    print(f"Solution to part 2: {result}")
